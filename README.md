0c0f5e16e166fe0065b0d756b621ce1bbc3af0c0
GabrieSVasc <vascgabrie@gmail.com>
2026-08-11 16:05:59 -0300

    Primeira entrega
    Entrega composta por dicionário de dados, arquivo de configuração do container, ddl e dml do banco e readme com as informações necessárias

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# qampus-bd
+# QAmpus BD
+## Integrantes
+[Divancy Bruno Torres Silva](https://github.com/DivBruno) \
+[Maria Gabriela da Silva Vasconcelos](https://github.com/GabrieSVasc) \
+[Otávio Alencar de Farias Marçal](https://github.com/otavio-alencar) \
+[Pedro Henrique da Silva Farias](https://github.com/Pedro-Henrique-Da-Silva-Farias) 
+
+## Contexto do projeto
+Projeto para implementação de um sistema web com foco em fórum acadêmico, o sistema tem como objetivo oferecer uma plataforma onde alunos possam, de forma rápida e prática, publicar dúvidas, responder perguntas e compartilhar conhecimento. Além disso, professores poderão utilizar o ambiente para esclarecer questões, divulgar eventos, projetos e comunicados acadêmicos.
+
+## Informações técnicas do projeto
+SGBD: MYSQL
+Usuário: admin
+Senha: senha123
+Nome do banco: qampus
+Porta: 3306
+
+## Dicionário de dados
+Disponível na pasta docs do repositório
+
+## Metodologia de povoamento
+O banco foi povoado através de um script DML com dados gerados por uma inteligência artificial. Essa metodologia foi escolhida por ser uma forma prática de obter um bom volume de dados com conexões plausíveis entre si.
---
+++ init-scripts/01-create-tables.sql
@@ -0,0 +1,99 @@
+CREATE TABLE IF NOT EXISTS Usuario (
+  id INT PRIMARY KEY Not Null,
+  email VARCHAR(100) Unique Not Null,
+  nome VARCHAR(100) Not Null,
+  senha VARCHAR(100) Not Null,
+  reputacao INT Not Null
+);
+
+CREATE TABLE IF NOT EXISTS Professor(
+  codigo VARCHAR(50) PRIMARY KEY Not Null,
+  idUsuario INT Not Null,
+  FOREIGN KEY (idUsuario) REFERENCES Usuario(id)
+);
+
+CREATE TABLE IF NOT EXISTS Curso(
+  id INT PRIMARY KEY Not Null,
+  bloco CHAR(1),
+  nome VARCHAR(50),
+  codCoordenador VARCHAR(50) Not Null,
+  FOREIGN KEY (codCoordenador) REFERENCES Professor(codigo)
+);
+
+CREATE TABLE IF NOT EXISTS Aluno(
+  matricula VARCHAR(50) PRIMARY KEY Not Null,
+  idUsuario INT Not Null,
+  idCurso INT Not Null,
+
+  FOREIGN KEY (idUsuario) REFERENCES Usuario(id),
+  FOREIGN KEY (idCurso) REFERENCES Curso(id)
+);
+
+CREATE TABLE IF NOT EXISTS Post(
+  id INT PRIMARY KEY Not Null,
+  conteudo VARCHAR(5000) Not Null,
+  data_postagem date Not Null,
+  titulo VARCHAR(150) Not Null,
+  idUsuario INT Not Null,
+  FOREIGN KEY (idUsuario) REFERENCES Usuario(id)
+);
+
+CREATE TABLE IF NOT EXISTS Comentario(
+  id INT PRIMARY KEY Not Null,
+  conteudo VARCHAR(1000) Not Null,
+  data_comentario date Not Null,
+  idUsuario INT Not Null,
+  idPost INT Not Null,
+
+  FOREIGN KEY (idUsuario) REFERENCES Usuario(id),
+  FOREIGN KEY (idPost) REFERENCES Post(id)
+);
+
+CREATE TABLE IF NOT EXISTS Voto(
+  id INT PRIMARY KEY Not Null,
+  tipo CHAR(1) Not Null,
+  idUsuario INT Not Null,
+  idPost INT Not Null,
+
+  FOREIGN KEY (idUsuario) REFERENCES Usuario(id),
+  FOREIGN KEY (idPost) REFERENCES Post(id)
+);
+
+CREATE TABLE IF NOT EXISTS Tag(
+  id INT PRIMARY KEY Not Null,
+  nome VARCHAR(50) Not Null
+);
+
+CREATE TABLE IF NOT EXISTS Tag_Post(
+  idTag INT Not Null,
+  idPost INT Not Null,
+  PRIMARY KEY(idTag, idPost),
+  FOREIGN KEY (idTag) REFERENCES Tag(id),
+  FOREIGN KEY (idPost) REFERENCES Post(id)
+);
+
+CREATE TABLE IF NOT EXISTS Disciplina(
+  codigo VARCHAR(50) PRIMARY KEY Not Null,
+  nome VARCHAR(50) Not Null,
+  idCurso INT Not Null,
+
+  FOREIGN KEY (idCurso) REFERENCES Curso(id)
+);
+
+CREATE TABLE IF NOT EXISTS Turma(
+  id INT PRIMARY KEY Not Null,
+  periodo CHAR(6) Not Null,
+  codDisciplina VARCHAR(50) Not Null,
+  codProfessor VARCHAR(50) Not Null,
+
+  FOREIGN KEY (codDisciplina) REFERENCES Disciplina(codigo),
+  FOREIGN KEY (codProfessor) REFERENCES Professor(codigo)
+);
+
+CREATE TABLE IF NOT EXISTS Estuda(
+  idTurma INT Not Null,
+  matrAluno VARCHAR(50) Not Null,
+  PRIMARY KEY(idTurma, matrAluno),
+  FOREIGN KEY (idTurma) REFERENCES Turma(id),
+  FOREIGN KEY (matrAluno) REFERENCES Aluno(matricula)
+);
